3819750143dd07dbbc208cf433a992728937c00d
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-07-01 21:48:41 -0400

    feat: added color pattern :sparkles:

---
+++ zuarte/lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LightColors {
+  final Color background = const Color(0xFFF9F9F9);
+  final Color primaryText = const Color(0xFF303030);
+  final Color secondaryText = const Color(0xFF666666);
+  final Color primaryAction = const Color(0xFF1DB954);
+  final Color details = const Color(0xFF4B0082);
+  final Color cardElements = const Color(0xFFE0E0E0);
+  final Color borders = const Color(0xFFBDBDBD);
+}
+
+class DarkColors {
+  final Color background = const Color(0xFF303030);
+  final Color primaryText = const Color(0xFFF9F9F9);
+  final Color secondaryText = const Color(0xFFAAAAAA);
+  final Color primaryAction = const Color(0xFF1DB954);
+  final Color details = const Color(0xFF4B0082);
+  final Color cardElements = const Color(0xFF2A2A2A);
+  final Color borders = const Color(0xFF505050);
+}
