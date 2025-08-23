d44753fcc4fda9eacc4a4e2fb9e41f30c36b90b3
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-08-22 20:35:13 -0400

    feat: added logic to check if the permissions needed to fetch music from the phone have been accepted :sparkles:

--- zuarte/lib/views/settings/settings_screen.dart
+++ zuarte/lib/views/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../constants/icons.dart';
@@ -71,8 +72,74 @@ class SettingsScreen extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(height: height * 0.02),
+          componentCard(
+            ctx: context,
+            height: height * 0.11,
+            padding: EdgeInsets.symmetric(horizontal: width * 0.04),
+            child: Row(
+              children: [
+                Iconify(
+                  AppIcons.padlock,
+                  color: iconColor(theme),
+                  size: iconSize(23),
+                ),
+                SizedBox(width: width * 0.02),
+                Text(
+                  'Alterar permissão',
+                  style: textStyle(
+                    size: 15,
+                    color: theme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  style: settingsButtonStyle(context),
+                  onPressed: () async {
+                    final OnAudioQuery audioQuery = OnAudioQuery();
+                    bool permission = await audioQuery.permissionsStatus();
+                    if (!permission) {
+                      permission = await audioQuery.permissionsRequest();
+                    } else {
+                      // ignore: use_build_context_synchronously
+                      alertDialog(context, theme);
+                    }
+                  },
+                  child: Text(
+                    'Alternar',
+                    style: textStyle(
+                      size: 15,
+                      color: theme.onPrimaryContainer,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  Future<void> alertDialog(BuildContext ctx, ColorScheme theme) {
+    return showDialog(
+      context: ctx,
+      builder: (context) {
+        return AlertDialog(
+          icon: Iconify(AppIcons.alert),
+          title: Text(
+            'Permissões já concedidas',
+            style: textStyle(
+              size: 15,
+              color: theme.primary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          titlePadding: EdgeInsets.only(bottom: 10.h, left: 10.w, right: 10.w),
+        );
+      },
     );
   }
 }
