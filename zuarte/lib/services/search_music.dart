74a0c6fefce8f81a2a9f681bcd23582d9f14d619
Yuri Gabriel Ferreira de Souza <yurigabrielsouza10@gmail.com>
2025-08-27 22:04:25 -0400

    feat: added logic to filter desired audios and sort them by order of creation :sparkles:

--- zuarte/lib/services/search_music.dart
+++ zuarte/lib/services/search_music.dart
@@ -3,10 +3,6 @@ import 'package:on_audio_query/on_audio_query.dart';
 final OnAudioQuery _audioQuery = OnAudioQuery();
 
 Future<List<SongModel>> searchMusic() async {
-  bool allowed = await _audioQuery.permissionsStatus();
-
-  if (!allowed) return [];
-
   List<SongModel> songs = await _audioQuery.querySongs(
     sortType: SongSortType.DATE_ADDED,
     orderType: OrderType.DESC_OR_GREATER,
